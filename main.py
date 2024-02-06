c639b5872f162b3a5e3d46d154d901fa67e8350b
Veregorn <rjbarco@gmail.com>
2024-02-06 17:58:08 +0100

    Now the provinces not guessed are saved in a CSV file (provinces to learn)

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def show_win_prompt():
 
     # Destroys the window after closing the message
     root.destroy()
+    
+    # Ends the program
+    exit()
 
 # Create the screen
 screen = turtle.Screen()
@@ -42,6 +45,8 @@ printer = turtle.Turtle()
 printer.hideturtle()
 printer.color("Black")
 printer.penup()
+printer.goto(-200, 400)
+printer.write("Type 'Exit' to quit the game and save the provinces you left in a CSV file", font=('Arial', 14, 'bold'))
 
 # We need a loop break condition
 all_provinces_guessed = False
@@ -57,6 +62,10 @@ while not all_provinces_guessed:
 
     # Convert the guess to Title case
     answer_province_titled = answer_province.title()
+
+    # If the user have typed 'Exit' quit the game
+    if answer_province_titled == 'Exit':
+        break
 
     # Check if the guess is among the 50 provinces
     row = provinces_data.loc[provinces_data['province'] == answer_province_titled]
@@ -78,6 +87,23 @@ while not all_provinces_guessed:
             if len(correct_guesses) == 50:
                 all_provinces_guessed = True
 
-show_win_prompt()
+# If the user has guessed all the provinces, show the winning prompt
+if all_provinces_guessed:
+    show_win_prompt()
 
-screen.exitonclick()
+# If not, the user have Exit the program manually, so we save missed provinces in a CSV file
+# First we need to separate not guessed provinces
+# Let's create a set from the column 'provinces' in the DataFrame
+all_provinces = set(provinces_data["province"])
+
+# Now let's create another set with the provinces guessed
+provinces_guessed = set(correct_guesses)
+
+# We use the operator ^ to have the diff of the 2 sets
+provinces_not_guessed = all_provinces ^ provinces_guessed
+print(provinces_not_guessed)
+
+# In order to create a CSV file, we need first to create a DataFrame
+my_df = pandas.DataFrame(list(provinces_not_guessed), columns=['Provinces not guessed'])
+
+my_df.to_csv("not_guessed_provinces.csv")
